353b7a731cdad69e624b7f7e1f508f8dd66e7230
d1sturrb <alexander.biber@outlook.com>
2020-10-29 15:45:27 +0100

    Final
    Final

--- alex.py
+++ alex.py
@@ -1,1 +1,43 @@
 
+#!/bin/python3
+
+import turtle, time
+import random
+
+elsa = turtle.Turtle()
+turtle.Screen().bgcolor("white")
+colours = ["green", "purple", "red", "tomato"]
+elsa.speed(0)
+
+elsa.penup()
+elsa.backward(100)
+elsa.left(45)
+elsa.pendown()
+    
+    
+def branch():
+    for i in range(3):
+        for i in range(3):
+            elsa.forward(30)
+            elsa.backward(30)
+            elsa.right(45)
+        elsa.left(90)
+        elsa.backward(30)
+        elsa.left(45)
+    elsa.right(90)
+    elsa.forward(90)
+    
+for i in range (4):
+    for i in range(8):
+            branch()
+            elsa.color(random.choice(colours))
+            elsa.left(45)
+            
+
+    elsa.penup()
+    elsa.forward(250)
+    elsa.right(270)
+    elsa.pendown()
+
+
+time.sleep(3)
