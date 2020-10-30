a714fa08e83ce186ed05b065fc3efafd8ed12383
Jasmin Bächle <jasmin-baechle@web.de>
2020-10-30 10:15:35 +0100

    update while

--- jasmin.py
+++ jasmin.py
@@ -16,24 +16,76 @@ elsa.left(45)
 elsa.pendown()
 
 #create one branch of the snowflake
-def branch():
-    for i in range(3):
-        for i in range(3):
-            elsa.forward(30)
-            elsa.backward(30)
-            elsa.right(45)
+#def branch():
+#    for i in range(3):
+#        for i in range(3):
+#            elsa.forward(30)
+#            elsa.backward(30)
+#            elsa.right(45)
+#        elsa.left(90)
+#        elsa.backward(30)
+#        elsa.left(45)
+#    elsa.right(90)
+#    elsa.forward(90)
+    
+#create one branch by using while-loops
+
+def whilebranch():
+    x = 0
+    while(x < 3):
+        elsa.forward(30)
+        elsa.backward(30)
+        elsa.right(45)
+        x += 1
+    x = 0
+    while(x < 1):
         elsa.left(90)
         elsa.backward(30)
         elsa.left(45)
+        x += 1
+    x = 0  
+    while(x < 3):
+        elsa.forward(30)
+        elsa.backward(30)
+        elsa.right(45)
+        x += 1
+    x = 0
+    while(x < 1):
+        elsa.left(90)
+        elsa.backward(30)
+        elsa.left(45)
+        x += 1
+    x = 0
+    while(x < 3):
+        elsa.forward(30)
+        elsa.backward(30)
+        elsa.right(45)
+        x += 1
+    x = 0
+    while(x < 1):
+        elsa.left(90)
+        elsa.backward(30)
+        elsa.left(45)
+        x += 1
     elsa.right(90)
     elsa.forward(90)
-
+    
 #draw branch 8 times
-for i in range(8):
-    branch()
+#for i in range(8):
+#    whilebranch()
+#    elsa.color(random.choice(colours))
+#    elsa.left(45)
+
+x= 0
+while (x < 8):
+    whilebranch()
     elsa.color(random.choice(colours))
     elsa.left(45)
+    x += 1
 
+
+
+turtle.Screen().exitonclick()
 
     
     
