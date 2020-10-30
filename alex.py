59da136119f8c0b238b1d0513685c672798f9a76
d1sturrb <alexander.biber@outlook.com>
2020-10-30 09:34:42 +0100

    finished

--- alex.py
+++ alex.py
@@ -17,35 +17,35 @@ elsa.pendown()
     
 def branch():
     
-    i1 = 0    
+    i1 = 0  # initialise i1 with int-value 0
     while i1 < 3:
         i2 = 0
-        while int2 < 3:
+        while i2 < 3:
             elsa.forward(30)
             elsa.backward(30)
             elsa.right(45)
+            i2 += 1
         elsa.left(90)
         elsa.backward(30)
         elsa.left(45)
-        i2 += 1
+        i1 += 1
     elsa.right(90)
     elsa.forward(90)
-    i1 += 1
     
 i3 = 0
 while i3 < 4:
     i4 = 0
     while i4 < 8:
-            branch()
-            elsa.color(random.choice(colours))
-            elsa.left(45)
-    i4 += 1
+        branch()
+        elsa.color(random.choice(colours))
+        elsa.left(45)
+        i4 += 1
 
     elsa.penup()
     elsa.forward(250)
     elsa.right(270)
     elsa.pendown()
-i3 += 1
+    i3 += 1
 
 
 time.sleep(3)
